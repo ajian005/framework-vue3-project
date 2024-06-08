250a49e4e4a5d902c318a9f8759178b63d0b1983
ajian005 <79331356@qq.com>
2024-06-08 22:10:44 +0800

    事件监听

--- src/views/studys/EventListener.vue
+++ src/views/studys/EventListener.vue
@@ -2,9 +2,22 @@
 import { ref } from 'vue'
 
 const count = ref(0)
+function increment() {
+  // 更新组件状态
+  count.value++
+}
 </script>
 
 <template>
   <!-- 使此按钮生效 -->
-  <button>Count is: {{ count }}</button>
+  <table>
+    <tr>
+      <button v-on:click="increment()">Count is: {{ count }}</button>
+    </tr>
+    <tr>
+      <button @click="increment()">Count is: {{ count }}</button>
+    </tr>
+  </table>
+  <!-- 使此按钮生效 -->
+  
 </template>
